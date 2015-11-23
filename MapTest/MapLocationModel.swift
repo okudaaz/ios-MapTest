96bbb231688d589151dcba269587abd4b6b52cb3
okudaaz <okuda@gloops.com>
2015-11-23 23:00:58 +0900

    Calcurate speed

---
+++ MapTest/MapLocationModel.swift
@@ -0,0 +1,19 @@
+//
+//  MapLocationModel.swift
+//  MapTest
+//
+//  Created by Eiji Okuda on 11/23/15.
+//  Copyright © 2015 Eiji Okuda. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+class MapLocationModel{
+    
+    var locations:[CLLocationCoordinate2D] = []
+    
+    var speed:[Double] = []
+        
+}
--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -13,7 +13,8 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     var myLocationManager:CLLocationManager!
-    var locations:[CLLocationCoordinate2D] = [];
+    var locationModel:MapLocationModel = MapLocationModel();
+    var lastDate:NSDate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +29,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         myLocationManager.startUpdatingLocation()
         
         mapView.delegate = self;
+        
+        lastDate = NSDate()
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,30 +48,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let longitude = newLocation.coordinate.longitude
         let latitude = newLocation.coordinate.latitude
         
-
         let location = CLLocationCoordinate2D(
             latitude: latitude,
             longitude: longitude
         )
 
-        locations.append(location)
-        
-        /*
-        let geodesic = MKGeodesicPolyline(coordinates: &locations[0], count: locations.count)
-        mapView.addOverlay(geodesic)
-        */
+        locationModel.locations.append(location)
 
-        let polyLine = MKPolyline(coordinates: &locations[0], count: locations.count)
-        mapView.addOverlay(polyLine)
-        
-        
-        print("Location = {\(latitude), \(longitude)}, #\(locations.count)")
-
-        if(locations.count == 1){
-            let span = MKCoordinateSpanMake(0.05, 0.05)
-            let region = MKCoordinateRegion(center: location, span: span)
-            mapView.setRegion(region, animated: true)
+        // Calcurate speed
+        let deltaSecond = NSDate().timeIntervalSinceDate(lastDate)
+        let distanceMeter = newLocation.distanceFromLocation(oldLocation)
+        let speed = (distanceMeter / 1000) / (deltaSecond / 3600)
+        if(locationModel.locations.count > 1){
+            locationModel.speed.append(speed)
+        } else {
+            locationModel.speed.append(0.0)
         }
+        
+        // Show Poly Line
+        showPolyLine()
+        
+        print("Location = {\(latitude), \(longitude)}, Distance = \(distanceMeter), Second = \(deltaSecond), Speed = \(speed), #\(locationModel.locations.count)")
+        
+        lastDate = NSDate()
         
         /*
         let annotation = MKPointAnnotation()
@@ -78,6 +80,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         mapView.addAnnotation(annotation)
         */
         
+    }
+
+    func showPolyLine(){
+        
+        let locations = locationModel.locations
+        
+        let polyLine = MKPolyline(coordinates: &locationModel.locations[0], count: locations.count)
+        mapView.addOverlay(polyLine)
+        
+        if(locations.count == 1){
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: locations[0], span: span)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
